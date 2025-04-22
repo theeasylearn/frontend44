91e7020ebddfe294e003c0d64f39503835b329be
theeasylearn <theeasylearn@gmail.com>
2025-04-22 19:44:08 +0530

    basic of reactjs

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+//Create virtual DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var address = <div>
+  <h1>the easylearn academy</h1>
+  <hr/>
+  105, Eva surbhi, opp aksharwadi temple <br/>
+  waghawadi road <br/> 
+  Bhavnagar -  364001 <br/>
+  {5+10}
+</div>
+var form = ( <form action="success.html" method="post">
+        <table border="1" align="center" cellpadding="10" width="50%">
+            <tr>
+                <td bgcolor="grey">
+                    <h1 align="center">Login</h1>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="email">Email address</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="email" name="email" id="email" required />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="password">Password</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="password" name="password" id="password" required />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="login" id="login" value="Sign in" />
+                    <input type="reset" name="clear" id="clear" value="clear all" />
+                </td>
+            </tr>
+        </table>
+    </form>)
+root.render(address);
